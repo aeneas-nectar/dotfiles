bd017f607427ce91dc6bdd6eac11de3c42294b54
awalkerca <aeneas_nectar@whalemail.ca>
2023-06-12 21:45:29 -0400

    get zsh-setup to symlink spaceship properly

--- profiles/linux_home/install/zsh-setup.sh
+++ profiles/linux_home/install/zsh-setup.sh
@@ -2,3 +2,6 @@
 
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/themes/spaceship-prompt" --depth=1
+
+
+ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
